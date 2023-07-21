db9ce59d2070b1a046f99e99b79d3fa4d2a991a6
guemax <github-mg@guenther.cloud>
2023-07-21 18:56:25 +0200

    Add model `Class`

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Class(models.Model):
+    class_level = models.IntegerField(verbose_name='Klassenstufe')
+    parallel_class = models.CharField(max_length=1, verbose_name='Parallelklasse')
+
+    class Meta:
+        verbose_name = 'Klasse'
+        verbose_name_plural = 'Klassen'
+
+    def __str__(self) -> str:
+        return str(self.class_level) + self.parallel_class
