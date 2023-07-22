4ef98eef913f2f4af4f2b0219eeaee35c93b1967
guemax <github-mg@guenther.cloud>
2023-07-22 08:03:20 +0200

    Register model `Assessment` on admin site

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Class, Station
+from .models import Class, Station, Assessment
+
+
+class AssessmentAdmin(admin.ModelAdmin):
+    list_display = ['class_name', 'station_name', 'score']
+
 
 admin.site.register(Class)
 admin.site.register(Station)
+admin.site.register(Assessment, AssessmentAdmin)
