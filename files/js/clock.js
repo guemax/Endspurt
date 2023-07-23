b3815fdc4ebecbdffd7cfefa40b9ec2205aaf4ec
guemax <github-mg@guenther.cloud>
2023-07-23 19:30:04 +0200

    Fix typo in clock.js

--- files/js/clock.js
+++ files/js/clock.js
@@ -1,15 +1,15 @@
 var hideColon = false;
-var seperator = ":";
+var separator = ":";
 
 function clock() {
-    seperator = hideColon ? " " : ":";
+    separator = hideColon ? " " : ":";
     hideColon = hideColon ? false : true;
 
     var d = new Date();
     hours = String(d.getHours()).padStart(2, "0");
     minutes = String(d.getMinutes()).padStart(2, "0");
     
-    var time = hours + seperator + minutes;
+    var time = hours + separator + minutes;
     document.querySelector("#clock").innerHTML = time;
     
     t = setTimeout(() => {
