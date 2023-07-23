39bcd08d2c97df8440087e892838e1e8ab8f3363
guemax <github-mg@guenther.cloud>
2023-07-23 15:25:26 +0200

    Add blinking colon for current time display

--- files/js/clock.js
+++ files/js/clock.js
@@ -1,7 +1,16 @@
+var hideColon = false;
+var seperator = ":";
+
 function clock() {
     var d = new Date();
-    document.querySelector("#clock").innerHTML = d.toLocaleTimeString("de-DE");
+    
+    seperator = hideColon ? ":" : " ";
+    hideColon = hideColon ? false : true;
+    
+    var time = d.getHours() + seperator + d.getMinutes();
+    
+    document.querySelector("#clock").innerHTML = time;
     t = setTimeout(() => {
 	clock();
-    }, 500);
+    }, 1000);
 }
