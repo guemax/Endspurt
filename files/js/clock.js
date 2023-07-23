e89f2bd0fa38d96c0f8e0af743f562de6848c00d
guemax <github-mg@guenther.cloud>
2023-07-23 19:26:48 +0200

    Pad hours and minutes of current time with zero
    Fixes #23

--- files/js/clock.js
+++ files/js/clock.js
@@ -2,14 +2,16 @@ var hideColon = false;
 var seperator = ":";
 
 function clock() {
-    var d = new Date();
-    
     seperator = hideColon ? ":" : " ";
     hideColon = hideColon ? false : true;
+
+    var d = new Date();
+    hours = String(d.getHours()).padStart(2, "0");
+    minutes = String(d.getMinutes()).padStart(2, "0");
     
-    var time = d.getHours() + seperator + d.getMinutes();
-    
+    var time = hours + seperator + minutes;
     document.querySelector("#clock").innerHTML = time;
+    
     t = setTimeout(() => {
 	clock();
     }, 1000);
